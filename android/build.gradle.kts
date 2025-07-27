26a8a69f18d795a5abec3de4a19b312c69849c8d
refath2024 <hosenrefath516@gmail.com>
2025-07-28 00:14:55 +0600

    user authentication done, also the registration data stored clear

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,9 +1,21 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.2")
+        classpath("com.google.gms:google-services:4.3.15")
+    }
+}
+
 allprojects {
     repositories {
         google()
         mavenCentral()
     }
 }
+
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
